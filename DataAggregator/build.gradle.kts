bd4851ff3597af70fcb83cc237c440af461eeefa
NightEule5 <24661563+NightEule5@users.noreply.github.com>
2021-01-04 02:40:49 -0700

    Removed an OAuth token. Woops!

--- DataAggregator/build.gradle.kts
+++ DataAggregator/build.gradle.kts
@@ -1,12 +1,5 @@
 repositories {
-	maven(url = "https://maven.pkg.github.com/coinapi/coinapi-sdk")
-	{
-		credentials.run()
-		{
-			username = "NightEule5"
-			password = "1cac22e065e47f42cb10c3d5e806600ed5cbbcaf"
-		}
-	}
+
 }
 
 dependencies {
